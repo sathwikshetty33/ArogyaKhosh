fa0abfb9249e3f4aa04a9eed7cf7ab472f206588
Sathwik Shetty <sathwikshetty2k5@gmail.com>
2026-09-14 17:34:41 +0000

    backend/db: Add migration helpers.

---
+++ services/backend/internal/db/migrations/migrations.go
@@ -0,0 +1,43 @@
+package migrations
+
+import (
+	"embed"
+	"fmt"
+
+	"github.com/go-gormigrate/gormigrate/v2"
+	"gorm.io/gorm"
+)
+
+//go:embed *.sql
+var files embed.FS
+
+func All() []*gormigrate.Migration {
+	return []*gormigrate.Migration{
+		sqlMigration("0001_init"),
+	}
+}
+
+func sqlMigration(id string) *gormigrate.Migration {
+	return &gormigrate.Migration{
+		ID: id,
+		Migrate: func(tx *gorm.DB) error {
+			return exec(tx, id+".up.sql")
+		},
+		Rollback: func(tx *gorm.DB) error {
+			return exec(tx, id+".down.sql")
+		},
+	}
+}
+
+func exec(tx *gorm.DB, name string) error {
+	body, err := files.ReadFile(name)
+	if err != nil {
+		return fmt.Errorf("read %s: %w", name, err)
+	}
+
+	if err := tx.Exec(string(body)).Error; err != nil {
+		return fmt.Errorf("exec %s: %w", name, err)
+	}
+
+	return nil
+}
